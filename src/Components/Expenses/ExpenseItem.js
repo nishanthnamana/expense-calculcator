032e3f848a4db9ceae56d316776d9af91abc757f
Nishanth Namana <nishanth.namana@ey.com>
2022-09-08 10:19:13 -0700

    Restructuring Expense Components

---
+++ src/Components/Expenses/ExpenseItem.js
@@ -0,0 +1,27 @@
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+import { useState } from "react";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.data.title);
+
+  const clickHandler = () => {
+    setTitle('Updated!!!!!');
+    console.log('Im Clicked!!!!');
+  }
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.data.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{props.data.amount}</div>
+        <button onClick={clickHandler}>Click Me</button>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,14 +1,15 @@
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem data={props.data[0]} />
       <ExpenseItem data={props.data[1]} />
       <ExpenseItem data={props.data[2]} />
       <ExpenseItem data={props.data[3]} />
-    </div>
+    </Card>
   );
 }
 
